14845f7e56e6e674752f309129989b3b33479a7d
Supra-teem <abir.bose14@gmail.com>
2021-08-23 16:09:22 +0530

    1st console app

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter a number to get factorial: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int fact = 1;
+            if(n == 0)
+            {
+                fact = 1;
+                Console.WriteLine("Factorial of " + n + " is " + fact);
+            }
+            else
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    fact = fact * i;
+
+
+                }
+                Console.WriteLine("Factorial of " + n + " is " + fact);
+            }
+        
         }
     }
 }
---
+++ ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 1, sum,i,n;
+            Console.Write("Enter the range: ");
+            n=Convert.ToInt32(Console.ReadLine());
+            Console.Write(a+" "+b+" ");
+            for(i=2;i<n;i++)
+            {
+                sum = a + b;
+                Console.Write(sum + " ");
+                a = b;
+                b = sum;
+
+
+            }
+        }
+    }
+}
