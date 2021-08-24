1dd36d8f927ee6ab91144e062ae2ce4836634b7b
Supra-teem <abir.bose14@gmail.com>
2021-08-24 12:42:55 +0530

    3rd commit

---
+++ Day2/1D_Array/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1D_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = new string[7];
+            Console.WriteLine("Enter Week Days one by one: ");
+            for(int i=0;i<7;i++)
+            {
+                arr[i] = Console.ReadLine();
+            }
+
+            for(int i=0;i<7;i++)
+            {
+                Console.Write(arr[i]+" ");
+            }
+        }
+    }
+}
