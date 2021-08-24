a5eb49169cbbba6c5da67bbc308d8af247bd5720
Supra-teem <abir.bose14@gmail.com>
2021-08-24 10:55:03 +0530

    Switch statement

---
+++ Day2/Switch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ch;
+            Console.Write("Enter your choice: ");
+            ch=Convert.ToInt32(Console.ReadLine());
+            switch(ch)
+            {
+                case 10:
+                    Console.WriteLine("You have choosen number 10");
+                    break;
+                case 20:
+                    Console.WriteLine("You have choosen number 20");
+                    break;
+                case 30:
+                    Console.WriteLine("You have choosen number 30");
+                    break;
+                default:
+                    Console.WriteLine("Please choose correct number!!!");
+                    break;
+            }
+        }
+    }
+}
