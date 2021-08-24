f925c93e1024539edee7fac5958324ccc095257b
Supra-teem <abir.bose14@gmail.com>
2021-08-24 13:14:13 +0530

    4rd commit

---
+++ Day2/Transpose _Matrix/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transpose__Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r, c, i, j;
+            Console.Write("Enter the number of rows: ");
+            r = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the number of Columns: ");
+            c = Convert.ToInt32(Console.ReadLine());
+            int[,] arr = new int[r, c];
+            Console.WriteLine("Enter the elements: ");
+            for(i=0;i<r;i++)
+            {
+                for(j=0;j<c;j++)
+                {
+                    arr[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("After transposing the array will be: ");
+            int[,] trans = new int[c, r];
+            for (i = 0; i < r; i++)
+            {
+                for (j = 0; j < c; j++)
+                {
+                    trans[j, i] = arr[i,j];
+                }
+            }
+            
+            for (i = 0; i < c; i++)
+            {
+                for (j = 0; j < r; j++)
+                {
+                    Console.Write(trans[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
