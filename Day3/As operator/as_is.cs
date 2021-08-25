bedccbea9fe29f7d90618eaa0534807e04bf0b75
Supra-teem <abir.bose14@gmail.com>
2021-08-25 13:18:24 +0530

    2nd commit

--- Day3/As operator/as_is.cs
+++ Day3/As operator/as_is.cs
@@ -6,29 +6,67 @@ using System.Threading.Tasks;
 
 namespace As_operator
 {
+    class class1
+    {
+
+    }
+    class class2
+    {
+
+    }
     class as_is
     {
+        public static void Test(object o)
+        {
+            class1 a;
+            class2 b;
+            if (o is class1)
+            {
+                Console.WriteLine("o is Class1");
+                a = (class1)o;
+            }
+            else if (o is class2)
+            {
+                Console.WriteLine("o is Class2");
+                b = (class2)o;
+            }
+            else
+            {
+                Console.WriteLine("o is neither Class1 nor Class2.");
+            }
+        }
+
         static void Main(string[] args)
         {
+            // As operator
             //string str1 = "Mindteck";
             //object obj1 = str1;
             //Console.WriteLine(obj1);
             //string str2 = obj1 as string;
             //Console.WriteLine(str2);
 
-            int[] arr = new int[2] { 10, 20 };
-            object obj1 = arr;
-            string str = obj1 as string;
-            if(str!=null)
-            {
-                Console.WriteLine("Successfull");
+            // As operator
+            //int[] arr = new int[2] { 10, 20 };
+            //object obj1 = arr;
+            //string str = obj1 as string;
+            //if(str!=null)
+            //{
+            //    Console.WriteLine("Successfull");
 
-            }
-            else
-            {
-                Console.WriteLine("Un Successfull");
-            }
-            Console.WriteLine("After conversion " + str);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Un Successfull");
+            //}
+            //Console.WriteLine("After conversion " + str);
+
+            class1 c1 = new class1();
+            class2 c2 = new class2();
+            Test(c1);
+            Test(c2);
+            Test("Passing String Value instead of class");
+
+
         }
     }
 }
---
+++ Day3/Exception Handling/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r, c,i,j,k=0;
+            try
+            {
+                Console.WriteLine("Enter the values of rows and columns: ");
+                r = Convert.ToInt32(Console.ReadLine());
+                c = Convert.ToInt32(Console.ReadLine());
+                int[,] a = new int[r, c];
+                int[] b = new int[r * c];
+                Console.WriteLine("Enter 2-D array elements: ");
+                for(i=0;i<r;i++)
+                {
+                    for(j=0;j<c;j++)
+                    {
+                        Console.Write("[{0}] [{1}]: ", i, j);
+                        a[i, j] = Convert.ToInt32(Console.ReadLine());
+                    }
+                }
+                for(i=0;i<r;i++)
+                {
+                    for(j=0;j<c;j++)
+                    {
+
+                        b[k++] = a[i, j];
+                    }
+                }
+                Console.WriteLine("Your 1-D array is: ");
+                for(i=0;i<(r*c);i++)
+                {
+                    Console.Write(b[i] + " ");
+
+                }
+                Console.WriteLine();
+                Console.WriteLine(b[10]);
+                Console.WriteLine();
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+            finally
+            {
+                Console.WriteLine("Thank you ");
+            }
+        }
+    }
+}
