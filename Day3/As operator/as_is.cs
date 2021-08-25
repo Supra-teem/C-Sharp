a595b0ab212101e545eb80b70c19f0ef97bc3951
Supra-teem <abir.bose14@gmail.com>
2021-08-25 11:05:40 +0530

    initial commit

---
+++ Day3/As operator/as_is.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace As_operator
+{
+    class as_is
+    {
+        static void Main(string[] args)
+        {
+            //string str1 = "Mindteck";
+            //object obj1 = str1;
+            //Console.WriteLine(obj1);
+            //string str2 = obj1 as string;
+            //Console.WriteLine(str2);
+
+            int[] arr = new int[2] { 10, 20 };
+            object obj1 = arr;
+            string str = obj1 as string;
+            if(str!=null)
+            {
+                Console.WriteLine("Successfull");
+
+            }
+            else
+            {
+                Console.WriteLine("Un Successfull");
+            }
+            Console.WriteLine("After conversion " + str);
+        }
+    }
+}
---
+++ Day3/Prog1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 65, 67, 68, 69 };
+            Char[] crr = new char[4];
+            int i = 0;
+            foreach(int item in arr)
+            {
+                crr[i] = (char)item;
+                i++;
+            }
+            foreach(char ch in crr)
+            {
+                Console.Write(ch + " ");
+            }
+
+
+        }
+    }
+}
