7987c95176a1490330e864df8b0e11202d1870cf
Supra-teem <abir.bose14@gmail.com>
2021-08-26 12:27:44 +0530

    4th Commit- area problem using dll

---
+++ Day4/ShapeArea/Area.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeArea
+{
+    public class Area
+    {
+        public void circleArea(double r)
+        {
+            Console.WriteLine("The area of the circle is " + (Math.PI * r * r));
+        
+        }
+        public void rhombusArea(double p, double q)
+        {
+            Console.WriteLine("The area of the Rhombus is " + (p*q)/2);
+
+        }
+
+    }
+}
---
+++ Day4/ShapeUsingdll/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShapeArea;
+namespace ShapeUsingdll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r, p, q;
+            Console.Write("Enter the radius of the circle: ");
+            Area a = new Area();
+            r = Convert.ToDouble(Console.ReadLine());
+            a.circleArea(r);
+            Console.WriteLine();
+            Console.WriteLine("Enter the value of P and Q of Rhombus: ");
+            p = Convert.ToDouble(Console.ReadLine());
+            q = Convert.ToDouble(Console.ReadLine());
+            a.rhombusArea(p, q);
+        }
+    }
+}
