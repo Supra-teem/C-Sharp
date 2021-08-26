05316da42dc58728944d2dab0f75a66c8e61c30e
Supra-teem <abir.bose14@gmail.com>
2021-08-26 11:09:55 +0530

    1st Commit

---
+++ Day4/Destructor/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestClass;
+
+namespace Destructor
+{
+    class test : IDisposable
+    {
+        public test()
+        {
+            Console.WriteLine("Object created");
+        }
+        ~test()
+        {
+            Console.WriteLine("Destructor called!!");
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(true);
+            Console.WriteLine("Dispose called!!");
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 0; i < 10; i++)
+            {
+                test t = new test();
+                t.Dispose();
+            }
+            //Class1 c= new Class1();
+            //c.welcome();
+
+        }
+    }
+}
