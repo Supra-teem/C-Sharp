69b57a78810a0835d020fc8f15d83a14c11beb09
Supra-teem <abir.bose14@gmail.com>
2021-08-26 15:18:52 +0530

    partial class

---
+++ Day4/Partial class/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Partial_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Partialdemo p = new Partialdemo();
+            p.fname = "Suprateem";
+            p.lname = "Bose";
+            p.displaydata();
+
+        }
+    }
+}
---
+++ Day4/Partial class/partial2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Partial_class
+{
+    public partial class Partialdemo
+    {
+        public void displaydata()
+        {
+            Console.WriteLine(@"the fnmae and lname  is {0}  {1}", _fname, _lname);
+
+
+        }
+
+    }
+}
