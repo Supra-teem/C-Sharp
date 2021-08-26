ee33931b066edfacb1f5eb65c37f6df35cc10c9b
Supra-teem <abir.bose14@gmail.com>
2021-08-26 13:14:37 +0530

    5th Commit- modifications in static,const and readonly

---
+++ Day4/static,Readonly,Const/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static_Readonly_Const
+{
+    class Program
+    {
+        int a;
+        static int b = 200;
+        const float pi = 3.14f;
+        readonly bool flag;
+        public Program(int a, bool flag)
+        {
+            this.a = a;
+            this.flag = flag;
+
+
+        }
+        public void incrementor()
+        {
+            a = a + 10;
+            b += 100;
+            //pi =3.12f;
+            //if (flag == true)
+            //{
+            //    flag = false;
+            //}
+            //else
+            //    flag = true;
+        }
+
+        static void Main(string[] args)
+        {
+            
+            Program p = new Program(50, true);
+            Program p1 = new Program(100, false);
+            Console.WriteLine(p.a + " " + p.flag+" "+b+ " "+pi);
+            p.incrementor();
+            Console.WriteLine(p.a + " " + p.flag+" "+b+ " "+pi);
+            Console.WriteLine(p1.a + " " + p1.flag + " " + b + " " + pi);
+            p1.incrementor();
+            Console.WriteLine(p1.a + " " + p1.flag + " " + b + " " + pi);
+            
+
+        }
+    }
+}
