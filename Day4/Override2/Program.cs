7ec87c02ac64444f7d99c876866c2a0b01fb94ce
Supra-teem <abir.bose14@gmail.com>
2021-08-26 16:01:51 +0530

    sealed class

---
+++ Day4/Override2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Override2
+{
+    class Transport
+    {
+        public void carHello()
+        {
+            Console.WriteLine("I'm a Car");
+        }
+        public virtual void carType()
+        {
+            Console.WriteLine("I'm A normal Car");
+        }
+    }
+
+    class Twowheeler : Transport
+    {
+        public override void carType()
+        {
+            Console.WriteLine("I'm a Two wheeler Car");
+        }
+    }
+    class Fourwheeler : Transport
+    {
+        public override void carType()
+        {
+            Console.WriteLine("I'm a Four wheeler Car");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Twowheeler c1 = new Twowheeler();
+            c1.carHello();
+            c1.carType();
+            Fourwheeler c2 = new Fourwheeler();
+            c2.carHello();
+            c2.carType();
+            Transport t = new Transport();
+            t.carHello();
+            t.carType();
+
+
+
+
+        }
+    }
+}
