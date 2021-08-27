be3ff3cb278a87bd66baa5462358f2e7f1ae72b8
Supra-teem <abir.bose14@gmail.com>
2021-08-27 11:14:11 +0530

    hash and stack

---
+++ Day5/hashtable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+namespace hashtable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable h = new Hashtable();
+            h.Add("ID", 1001);
+            h.Add("name", "Deepti");
+            h.Add("salary", 200);
+            h.Add("dept", "IT");
+            foreach (object o in h.Keys)
+            {
+                Console.WriteLine(h[o]);
+            }
+            Console.WriteLine("Salary= " + h["salary"]);
+
+        }
+    }
+}
