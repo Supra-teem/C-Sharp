f409e48cbac003d53ec6b9c1bde7e61a2b6420fa
Supra-teem <abir.bose14@gmail.com>
2021-08-31 16:07:46 +0530

    liskovs Principal

---
+++ Day6/liskovs principal/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace liskovs_principal
+{
+    public abstract class fruit
+    {
+        public abstract void color();
+    }
+    class apple : fruit
+    {
+        public override void color()
+        {
+            Console.WriteLine("Color is red");
+        }
+
+    }
+    class orange : fruit
+    {
+        public override void color()
+        {
+            Console.WriteLine("Color is orange");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //apple ap = new apple();
+            //ap.color();
+            //apple or = new orange();
+            //or.color();
+            fruit ap = new apple();
+            ap.color();
+            fruit or = new orange();
+            or.color();
+        }
+    }
+}
