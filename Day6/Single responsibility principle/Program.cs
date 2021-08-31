8f8d174294fc66396b838050c26840f7a5f36963
Supra-teem <abir.bose14@gmail.com>
2021-08-31 16:07:17 +0530

    some modification in SRP

--- Day6/Single responsibility principle/Program.cs
+++ Day6/Single responsibility principle/Program.cs
@@ -14,7 +14,7 @@ namespace Single_responsibility_principle
     }
     public interface IInterest
     {
-        void calculate(int b);
+        void calculate();
     }
 
     class Transac : Itransac
@@ -47,35 +47,82 @@ namespace Single_responsibility_principle
 
         }
     }
-    class Calculate : IInterest
-    { 
-        public void calculate(int balance)
+    //class Calculate : IInterest
+    //{ 
+    //    public void calculate(int balance)
+    //    {
+    //        int t;
+    //        Console.WriteLine("Enter the number of year: ");
+    //        t = Convert.ToInt32(Console.ReadLine());
+    //        //Console.WriteLine("Enter Your Balance: ");
+    //        //balance = Convert.ToInt32(Console.ReadLine());
+    //        double interest = (balance * t * 6) / 100;
+    //        Console.WriteLine("Your Interest is: "+interest);
+    //        Console.WriteLine("Your total balance would be :"+(balance+interest));
+
+    //    }
+
+    //}
+
+    class SavingAcc : IInterest
+    {
+        public void calculate()
         {
-            int t;
-            Console.WriteLine("Enter the number of year: ");
-            t = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("Enter Your Balance: ");
-            //balance = Convert.ToInt32(Console.ReadLine());
-            double interest = (balance * t * 6) / 100;
-            Console.WriteLine("Your Interest is: "+interest);
-            Console.WriteLine("Your total balance would be :"+(balance+interest));
+            Console.WriteLine("Enter the principal: ");
+            int p = Convert.ToInt32(Console.ReadLine());
+            int r = 6;
+            Console.WriteLine("Enter the time: ");
+            int t = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your Amount of Interst is: " + ((p * t * r) / 100));
 
         }
-
     }
+    class SalAcc : IInterest
+    {
+        public void calculate()
+        {
+            Console.WriteLine("Enter the principal: ");
+            int p = Convert.ToInt32(Console.ReadLine());
+            int r = 7;
+            Console.WriteLine("Enter the time: ");
+            int t = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your Amount of Interst is: " + ((p * t * r) / 100));
+
+        }
+    }
+    class ChildAcc : IInterest
+    {
+        public void calculate()
+        {
+            Console.WriteLine("Enter the principal: ");
+            int p = Convert.ToInt32(Console.ReadLine());
+            int r = 4;
+            Console.WriteLine("Enter the time: ");
+            int t = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your Amount of Interst is: " + ((p * t * r) / 100));
+
+        }
+    }
+
     class Program
     {
         
         static void Main(string[] args)
         {
-            
-            Transac t = new Transac();
-            t.credit(500);
-            t.credit(500);
-            t.debit(1000);
-            t.debit(6000);
-            Calculate c = new Calculate();
-            c.calculate(t.balance);
+
+            //Transac t = new Transac();
+            //t.credit(500);
+            //t.credit(500);
+            //t.debit(1000);
+            //t.debit(6000);
+            //Calculate c = new Calculate();
+            //c.calculate(t.balance);
+            SavingAcc sv = new SavingAcc();
+            SalAcc sl = new SalAcc();
+            ChildAcc ch = new ChildAcc();
+            sv.calculate();
+            sl.calculate();
+            ch.calculate();
 
         }
     }
